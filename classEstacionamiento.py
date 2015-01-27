3239fe0478f05812d448ce7dbc9fd827c4935c90
Guti <gustavogut1993@gmail.com>
2015-01-27 17:27:35 -0430

    3era iteracion

--- classEstacionamiento.py
+++ classEstacionamiento.py
@@ -3,4 +3,8 @@ class Estacionamiento(object):
 		self.reservaciones = []
 
 	def reservar(self, hInicio, hFinal):
-		return True
+		if len(self.reservaciones) == 0:
+			self.reservaciones.append((hInicio,hFinal))
+			return True
+		else :
+			return False
--- testEstacionamiento.py
+++ testEstacionamiento.py
@@ -10,7 +10,11 @@ class TestReservar(unittest.TestCase):
 		e = Estacionamiento()
 		e.reservar(800, 1000)
 
-
+	def testRealizarReservaEstacionamientoLleno(self):
+		e = Estacionamiento()
+		for i in range(10):
+			e.reservar(800,900)
+		assert not e.reservar(800,900)
 
 
 
