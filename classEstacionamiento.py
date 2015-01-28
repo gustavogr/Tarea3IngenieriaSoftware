5d1f058ead9dd444f59774bc51c8f8d2aeb4b9db
Genessis Sanchez <genny@genny.ac.labf.usb.ve>
2015-01-27 21:36:15 -0430

    cuarta iteracion de TDD

--- classEstacionamiento.py
+++ classEstacionamiento.py
@@ -1,10 +1,11 @@
 class Estacionamiento(object):
-	def __init__(self):
+	def __init__(self, capacidad):
 		self.reservaciones = []
+		self.capacidad = capacidad
 
 	def reservar(self, hInicio, hFinal):
-		if len(self.reservaciones) == 0:
+		if len(self.reservaciones) < self.capacidad:
 			self.reservaciones.append((hInicio,hFinal))
 			return True
-		else :
+		else:
 			return False
--- testEstacionamiento.py
+++ testEstacionamiento.py
@@ -3,19 +3,24 @@ from classEstacionamiento import *
 
 class TestReservar(unittest.TestCase):
 	def testExisteFuncion(self):
-		e = Estacionamiento()
-		e.reservar(0,0)
+		e = Estacionamiento(10)
+		e.reservar(0, 0)
 
-	def testRealizarReservaEstacionamientoVacio(self):
-		e = Estacionamiento()
-		e.reservar(800, 1000)
+	def testReservaEstacionamientoVacio(self):
+		e = Estacionamiento(10)
+		assert e.reservar(800, 1000)
 
-	def testRealizarReservaEstacionamientoLleno(self):
-		e = Estacionamiento()
+	def testReservaHorarioLleno(self):
+		e = Estacionamiento(10)
 		for i in range(10):
-			e.reservar(800,900)
-		assert not e.reservar(800,900)
+			e.reservar(800, 900)
+		assert not e.reservar(800, 900)
 
+	def testReservarHorarioNoVacio(self):
+		e = Estacionamiento(10)
+		for i in range(9):
+			e.reservar(800, 900)
+		assert e.reservar(800, 900)
 
 
 if __name__ == "__main__":
